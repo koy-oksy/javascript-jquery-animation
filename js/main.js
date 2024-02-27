3f459f22a7289ee26fc9e54d2913f31b583bc389
Ksu <85317684+koy-oksy@users.noreply.github.com>
2024-02-27 23:45:39 +0200

    javascript

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+
+jQuery(document).ready(function ($) {
+
+  $("#animated-link").click(function (e) {
+    console.log('clicked at link');
+        e.preventDefault(); // Зупиняємо перехід за замовчуванням
+        $(this).switchClass( "", "ml-10", 1000, "easeInOutQuad", function() {
+            // Після анімації, переходимо на нову сторінку
+             window.location.href = $(this).attr('href');
+        });
+    });
+  $("#animated-icon").click(function () {
+      console.log('clicked at icon');
+      $(this).animate({
+        marginright: '-=100px' // Зміщення іконки вліво на 100px
+      }, 1000); // тривалість анімації - 1 секунда
+    });
+
+});
